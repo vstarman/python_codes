b98cb66d3176a0c9900c809a59636fe5d95b3612
vstarman <vstarman@foxmail.com>
2018-01-08 09:46:04 +0800

    Quick sort

--- 00.算法入门/快速排序.py
+++ 00.算法入门/快速排序.py
@@ -1,36 +1,5 @@
 test = [3, 4, 12312, 3, 412, 12, 123, 2]
 
-# def sum_list(test):
-#     if len(test) == 0:
-#         return 0
-#     else:
-#         return test.pop() + sum_list(test)
-#
-#
-# result = sum_list(test)
-# print(result)
-
-
-'''
-def sort_list(lst):
-    if len(lst) < 2:
-        return lst
-    else:
-        value = lst.pop()
-        mid_lst = [value]
-        min_lst = list()
-        max_lst = list()
-        for i in lst:
-            if i < value:
-                min_lst.append(i)
-            elif i > value:
-                max_lst.append(i)
-            else:
-                mid_lst.append(i)
-        return sort_list(min_lst) + mid_lst + sort_list(max_lst)
-b = sort_list(test)
-print(b)'''
-
 
 def quick_sort(array):
     if len(array) < 2:
--- 00.算法入门/枚举法.py
+++ 00.算法入门/枚举法.py
@@ -1,12 +1,12 @@
 import time
 start_time = time.time()
-for a in range(0,1001):
-    for b in range(0, 1001):
-        for c in range(0, 1001):
-            if a ** 2 + b ** 2 == c ** 2 and a + b + c ==1000:
-                print(a,b,c)
-end_time = time.time()
-print("use time:", end_time - start_time)
+# for a in range(0,1001):
+#     for b in range(0, 1001):
+#         for c in range(0, 1001):
+#             if a ** 2 + b ** 2 == c ** 2 and a + b + c ==1000:
+#                 print(a, b, c)
+# end_time = time.time()
+# print("use time:", end_time - start_time)
 
 """
 0 500 500
@@ -15,3 +15,12 @@ print("use time:", end_time - start_time)
 500 0 500
 use time: 1697.7197799682617
 """
+
+for a in range(1001):
+    for b in range(1001-a):
+        c = 1000-a-b
+        if a**2 + b**2 == c**2 and a+b+c == 1000:
+            print('(a,b,c): {}'.format((a, b, c)))
+
+end_time = time.time()
+print('Use time: {}s'.format(end_time-start_time))
